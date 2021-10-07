b52bbe39588bd0ae314b075d02c60544b27f6e47
Ryan Lee <rjlguitar@gmail.com>
2021-10-07 08:08:03 -0700

    fixed import

--- dice-game/src/dice.js
+++ dice-game/src/dice.js
@@ -1,14 +1,14 @@
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { roll, reset } from '..actions/index';
+import { roll, reset } from './actions/index';
 
 function Dice() {
-  const result = useSelector((state) => state.rollReducer);
+  const dice = useSelector((state) => state.rollReducer);
   const dis = useDispatch();
   return (
     <div className='diceroll'>
-      <h3>{result}</h3>
-      <p>You rolled a {result}!</p>
+      <h3>{dice}</h3>
+      <p>You rolled a {dice}!</p>
       <button onClick={() => dis(roll())}>Roll</button>
       <button onClick={() => dis(reset())}>Reset</button>
     </div>
