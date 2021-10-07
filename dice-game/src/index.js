da30ea6e54080b7b75772050b3aaa478cc472fba
Ryan Lee <rjlguitar@gmail.com>
2021-10-07 08:07:55 -0700

    fixed store import directory

--- dice-game/src/index.js
+++ dice-game/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import allReducers from '../reducers/index';
+import allReducers from './reducers/index';
 import { Provider } from 'react-redux';
 
 const store = createStore(allReducers);
