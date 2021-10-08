2b3041d1ab39bf0f2cec283e103c28ed75566928
Ryan Lee <rjlguitar@gmail.com>
2021-10-08 13:33:09 -0700

    added d20 to display

--- dice-game/src/dice.js
+++ dice-game/src/dice.js
@@ -1,18 +1,25 @@
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { roll, reset } from './actions/index';
+import { roll, reset, rolld20 } from './actions/index';
 import './dice.css';
 
 function Dice() {
   const dice = useSelector((state) => state.rollReducer);
+  const d20dice = useSelector((state) => state.d20Reducer);
   const dis = useDispatch();
   return (
     <div className='diceroll'>
       <h2>Dice Roller 🎲</h2>
-      <div className='diceborder'>
-        <h3>{dice}</h3>
+      <div className='dicebox'>
+        <div className='diceborder'>
+          <h3>{dice}</h3>
+        </div>
+        <div className='d20border'>
+          <h3>{d20dice}</h3>
+        </div>
       </div>
-      <button onClick={() => dis(roll())}>Roll</button>
+      <button onClick={() => dis(roll())}>Roll D6</button>
+      <button onClick={() => dis(rolld20())}>Roll D20</button>
       <button onClick={() => dis(reset())}>Reset</button>
     </div>
   );
