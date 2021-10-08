00eca3af7ece80a346d469cb43586aa1d5930190
Ryan Lee <rjlguitar@gmail.com>
2021-10-08 13:33:01 -0700

    added d20 reducer to allReducers

--- dice-game/src/reducers/index.js
+++ dice-game/src/reducers/index.js
@@ -1,8 +1,10 @@
 import rollReducer from './rollReducer';
+import d20Reducer from './d20Reducer';
 import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
   rollReducer,
+  d20Reducer,
 });
 
 export default allReducers;
